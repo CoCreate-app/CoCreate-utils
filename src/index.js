ec65c2088e85aefd21c52d5f7878fa79113e8fd2
frank <frank@cocreate.app>
2021-05-21 03:17:30 +0000

    clean tree

--- src/index.js
+++ src/index.js
@@ -4,60 +4,60 @@
  */
 
 
-export function generateUUID(length = 36) {
-  // if (length == 10) {
-  //   var result           = '';
-  //   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //   var charactersLength = characters.length;
-  //   for ( var i = 0; i < length; i++ ) {
-  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  //   }
+// export function generateUUID(length = 36) {
+//   // if (length == 10) {
+//   //   var result           = '';
+//   //   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   //   var charactersLength = characters.length;
+//   //   for ( var i = 0; i < length; i++ ) {
+//   //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   //   }
 
-  //   var dd = new Date().toTimeString();
-  //   var random = dd.replace(/[\W_]+/g, "").substr(0,6);
-  //   result += random;
-  //   return result;
-  // }
+//   //   var dd = new Date().toTimeString();
+//   //   var random = dd.replace(/[\W_]+/g, "").substr(0,6);
+//   //   result += random;
+//   //   return result;
+//   // }
 
-  let d = new Date().getTime();
-  let d2 =
-    (window.performance &&
-      window.performance.now &&
-      window.performance.now() * 1000) ||
-    0;
-  let pattern = "uxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+//   let d = new Date().getTime();
+//   let d2 =
+//     (window.performance &&
+//       window.performance.now &&
+//       window.performance.now() * 1000) ||
+//     0;
+//   let pattern = "uxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
 
-  if (length <= pattern.length) {
-    pattern = pattern.substr(0, length);
-  }
-  else {
-    let add_len = length - pattern.length;
-    let sub_pattern = "-xxxyyxxx";
+//   if (length <= pattern.length) {
+//     pattern = pattern.substr(0, length);
+//   }
+//   else {
+//     let add_len = length - pattern.length;
+//     let sub_pattern = "-xxxyyxxx";
 
-    let group_n = Math.floor(add_len / sub_pattern.length);
+//     let group_n = Math.floor(add_len / sub_pattern.length);
 
-    for (let i = 0; i < group_n; i++) {
-      pattern += sub_pattern;
-    }
+//     for (let i = 0; i < group_n; i++) {
+//       pattern += sub_pattern;
+//     }
 
-    group_n = add_len - group_n * sub_pattern.length;
-    pattern += sub_pattern.substr(0, group_n);
-  }
+//     group_n = add_len - group_n * sub_pattern.length;
+//     pattern += sub_pattern.substr(0, group_n);
+//   }
 
-  let uuid = pattern.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16;
-    if (d > 0) {
-      var r = (d + r) % 16 | 0;
-      d = Math.floor(d / 16);
-    }
-    else {
-      var r = (d2 + r) % 16 | 0;
-      d2 = Math.floor(d2 / 16);
-    }
-    return (c == "x" ? r : (r & 0x7) | 0x8).toString(16);
-  });
-  return uuid;
-}
+//   let uuid = pattern.replace(/[xy]/g, function(c) {
+//     var r = Math.random() * 16;
+//     if (d > 0) {
+//       var r = (d + r) % 16 | 0;
+//       d = Math.floor(d / 16);
+//     }
+//     else {
+//       var r = (d2 + r) % 16 | 0;
+//       d2 = Math.floor(d2 / 16);
+//     }
+//     return (c == "x" ? r : (r & 0x7) | 0x8).toString(16);
+//   });
+//   return uuid;
+// }
 
 export function getParentFromElement(element, parent_class, attributes) {
   if (parent_class) {
@@ -314,6 +314,5 @@ export default {
   checkValue,
   getAttributes,
   isJsonString,
-  getParentFromElement,
-  generateUUID
+  getParentFromElement
 }
