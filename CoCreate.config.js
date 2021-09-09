197bbcf6dc27b7db1a4f6bae623138075b12e192
frank pagan <frank@cocreate.app>
2021-09-09 02:15:33 +0000

    fix: CoCreate.config directory, path, name and  public

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,47 +1,42 @@
 module.exports = {
-    config: {
-        apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-        securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
-        organization_Id: "5de0387b12e200ea63204d6c",
-        host: "server.cocreate.app:8088"
+    "config": {
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_Id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
     },
- 
-    sources: [{
-            path: "./docs/index.html",
-            collection: "files",
-            document_id: "",
-            key: "html",
-            data:{
-                name: "Utils Doc",
-            }
-        },
-    ],
-   
-    crud: [{
-            collection: "routes",
-            document_id: "",
-            data:{
-                collection: "files",
-                document_id: "",
-                name: "html",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                route: "/docs/utils",
+    
+    "sources": [{
+            "entry": "./docs/index.html",
+            "collection": "files",
+            "document_id": "",
+            "key": "src",
+            "data":{
+                "name": "index.html",
+                "path": "/docs/utils/index.html",
+                "domains": [
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/utils",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "61381ed8829b690010a4f2b2"
             }
         }
     ],
-    
-    extract: {
-        directory: "./src/",
-        extensions: [
-            "js",
-            "css",
-            "html"
-        ],
-        ignores: [
-            "node_modules",
-            "vendor",
-            "bower_components",
-            "archive"
-        ],
-    }
+
+	"extract": {
+		"directory": "./src/",
+		"extensions": [
+			"js",
+			"css",
+			"html"
+		],
+		"ignores": [
+			"node_modules",
+			"vendor",
+			"bower_components",
+			"archive"
+		]
+	}
 }
+
