4aa204dc45c7010491a606f0377ad02e53dcf110
Frank Pagan <frank@cocreate.app>
2022-07-25 22:12:47 +0000

    added deprciated code for reference

--- src/index.old.js
+++ src/index.old.js
@@ -425,7 +425,24 @@ async function complexSelector(comSelector, callback) {
     }
   }
   
-  
+  function decodeArray(data) {
+		let keys = Object.keys(data);
+		let objectData = {};
+
+		keys.forEach((k) => {
+			let nk = k
+			if (/\[([0-9]*)\]/g.test(k)) {
+				nk = nk.replace(/\[/g, '.');
+				if (nk.endsWith(']'))
+					nk = nk.slice(0, -1)
+				nk = nk.replace(/\]./g, '.');
+				nk = nk.replace(/\]/g, '.');
+			}
+			objectData[nk] = data[k];
+		});
+		return objectData;
+	}
+
   
 export default {
   getElementPath,
