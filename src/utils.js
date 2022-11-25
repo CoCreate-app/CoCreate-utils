f8ad7913e5d0c658ca3ef58bda09082dde8b67ef
Frank Pagan <frank@cocreate.app>
2022-11-25 09:32:03 +0000

    feat: utils can be used in browser or server

---
+++ src/utils.js
@@ -0,0 +1,283 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+        	return factory(true)
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(false);
+    } else {
+        root.returnExports = factory(true);
+  	}
+}(typeof self !== 'undefined' ? self : this, function (isBrowser) {
+
+
+    /*globals DOMParser*/
+    function clickedElement() {
+        document.addEventListener('click', e => {
+            document.clickedElement = e.target;
+        });
+        let frameDocuments = window.top.frameDocuments;
+        if (!frameDocuments){
+            window.top.frameDocuments = new Map();
+            frameDocuments = window.top.frameDocuments;
+        }
+        let frames = document.querySelectorAll('iframe');
+        for (let frame of frames){
+            let frameDocument = frame.contentDocument;
+            if (!frameDocuments.has(frameDocument)){
+                frameDocuments.set(frameDocument, '')
+                frameDocument.addEventListener('click', e => {
+                    frameDocument.clickedElement = e.target;
+                });
+            }
+        }
+    }
+
+    function dotNotationToObject(data, obj = {}) {
+        try {	
+            for (const [key, value] of Object.entries(data)) {	
+                let newObject = obj
+                let oldObject = new Object(obj)
+                let keys = key.split('.');
+                let length = keys.length - 1
+                for (let i = 0; i < keys.length; i++) {
+                    if (/\[([0-9]*)\]/g.test(keys[i])){
+                        let [k, index] = keys[i].split('[');
+                        index = index.slice(0, -1)
+                        if (length == i){
+                            newObject[k] = oldObject[k] || [];
+                            newObject[k][index] = value;
+                        }
+                        else {
+                            newObject[k] = oldObject[k] || [];
+                            newObject[k][index] = oldObject[k][index] || {};
+                        }
+                        newObject = newObject[k][index]
+                        oldObject = oldObject[k][index]
+                    }
+                    else {
+                        if (length == i)
+                            newObject[keys[i]] = value;
+                        else
+                            newObject[keys[i]] = oldObject[keys[i]] || {};
+
+                        newObject = newObject[keys[i]]
+                        oldObject = oldObject[keys[i]]
+                    }
+                }
+            }
+            return obj
+        } catch (error) {
+            console.log("Error converting dot notation to object", error);
+            return false;
+        }
+    }
+
+    function parseTextToHtml(text) {
+    let doc = new DOMParser().parseFromString(text, "text/html");
+    if (doc.head.children[0]) return doc.head.children[0];
+    else return doc.body.children[0];
+    }
+
+    function cssPath(node, container) {
+        let pathSplits = [];
+        do {
+            if (!node || !node.tagName) return false;
+            let pathSplit = node.tagName.toLowerCase();
+            // if (node.tagName == "DOM-PARSER" || node.hasAttribute('contenteditable')){
+            //     pathSplit = "[contenteditable]";
+            //     node = '';
+            // }
+            if (node.id){
+                pathSplit += "#" + node.id;
+                node = '';
+            }
+            else {
+                // let eid = node.getAttribute('eid');
+                // if (/{{\s*([\w\W]+)\s*}}/g.test(eid)) {
+                // 	eid = false;
+                // }
+                // if (eid) {
+                //     pathSplit += `[eid="${eid}"]`;
+                //     node = '';
+                // }
+                // else {
+                    // if (node.classList.length) {
+                    //     node.classList.forEach((item) => {
+                    //         if (item.indexOf(":") === -1) pathSplit += "." + item;
+                    //     });
+                    // }
+            
+                    if (node.parentNode && node.parentNode.children.length > 1) {
+                        // ToDo: improve array logic so ignores javascript generated html??
+                        let children = []
+                        for (let child of node.parentNode.children){
+                            // if (!child.matches('.mirror'))
+                            //     children.push(child);
+                            if (child.tagName == node.tagName)
+                                children.push(child);
+                        }
+                        let index = Array.prototype.indexOf.call(
+                            children,
+                            node
+                        );
+                        // if (children.length > 1)
+                        // pathSplit += `:nth-child(${index + 1})`;
+                        pathSplit += `:nth-of-type(${index + 1})`;
+                    }
+            
+                    // pathSplits.unshift(pathSplit);
+                    node = node.parentNode;
+                    if (node == null || node.tagName == "HTML" || node.tagName == "DOM-PARSER" || node.nodeName == "#document" || node.hasAttribute('contenteditable'))
+                        node = '';
+                }
+            // }
+            pathSplits.unshift(pathSplit);
+        } while (node);
+        let path = pathSplits.join(" > ")
+        if (path && path.includes('<')) {
+            let index = path.lastIndexOf(' >')
+            if (index != -1)
+                path = path.slice(0, index)
+            else{
+                index = path.lastIndexOf('<')
+                path = path.slice(0, index)
+            }
+        }
+
+        return path;
+    }
+
+    function domParser(str) {
+        try {
+            var mainTag = str.match(/\<(?<tag>[a-z0-9]+)(.*?)?\>/).groups.tag;
+        } catch (e){
+            // console.log(e, 'find position: can not find the main tag');
+        }
+        let doc;
+        switch (mainTag) {
+            case 'html':
+                doc = new DOMParser().parseFromString(str, "text/html");
+                return doc.documentElement;
+            case 'body':
+                doc = new DOMParser().parseFromString(str, "text/html");
+                return doc.body;
+            case 'head':
+                doc = new DOMParser().parseFromString(str, "text/html");
+                return doc.head;
+
+            default:
+                let con = document.createElement('dom-parser');
+                con.innerHTML = str;
+                return con;
+        }
+    }
+
+    function queryDocumentSelectorAll(selector) {
+        let elements = [];
+
+        if (selector) {
+            let selectors = [selector];
+            if (selector.indexOf(',') !== -1){
+                selectors = selector.split(',');
+            }
+            for (let selector of selectors){
+                let els;
+                if (selector.indexOf(';') !== -1) {
+                    let targetDocument;
+                    let [documentSelector, targetSelector] = selector.split(';');
+                    if (['parent', 'parentDocument'].includes(documentSelector))
+                        targetDocument = window.parent.document;
+                    else {
+                        let frame = document.querySelector(documentSelector);
+                        if (frame)
+                            targetDocument = frame.contentDocument;
+                    }
+                    if (targetDocument){
+                        if (targetSelector)
+                            els = targetDocument.querySelectorAll(targetSelector);
+                        else
+                            if (targetDocument.clickedElement)
+                                els = [targetDocument.clickedElement];
+                    }
+                }
+                else
+                    els = document.querySelectorAll(selector);
+                if (els){
+                    els = Array.prototype.slice.call(els);
+                    elements = elements.concat(els);
+                }
+            }
+            return elements;
+        }
+    }
+
+    function queryDocumentSelector(selector) {
+        if (selector) {
+            let selectors = [selector];
+            if (selector.indexOf(',') !== -1){
+                selectors = selector.split(',');
+            }
+            for (let selector of selectors){
+                let el;
+                if (selector.indexOf(';') !== -1) {
+                    let targetDocument;
+                    let [documentSelector, targetSelector] = selector.split(';');
+                    if (['parent', 'parentDocument'].includes(documentSelector))
+                        targetDocument = window.parent.document;
+                    else {
+                        let frame = document.querySelector(documentSelector);
+                        if (frame)
+                            targetDocument = frame.contentDocument;
+                    }
+                    if (targetDocument){
+                        if (targetSelector)
+                            el = targetDocument.querySelector(targetSelector);
+                        else
+                            if (targetDocument.clickedElement)
+                                el = [targetDocument.clickedElement];
+                    }
+                }
+                else
+                    el = document.querySelector(selector);
+                if (el)
+                    return el
+            }
+            return;
+        }
+    }
+
+    // function computeStyles(el, properties) {
+    //   let computed = window.getComputedStyle(el);
+    //   let result = {};
+    //   properties.forEach((property) => {
+    //     result[property] = parseInt(computed[property]);
+    //   });
+    //   return result;
+    // }
+
+    // 	function checkParent(element, selectors){
+    // 	    let parentElement;
+    // 	    do {
+    // 	    	parentElement = element.parentElement.closest(selectors);
+    // 	    	if (parentElement) {
+    // 		    	element = parentElement;
+    // 		    } else {
+    // 				return element;
+    // 		    }
+    // 	    } while (parentElement);
+    // 	}
+
+    if (isBrowser)
+        clickedElement();
+
+    return {
+        parseTextToHtml,
+        dotNotationToObject,
+        cssPath,
+        domParser,
+        queryDocumentSelector,
+        queryDocumentSelectorAll
+    }
+
+}));
