a0ef76d24969e3679159987b2f3c4633bc46c138
Frank Pagan <frank@cocreate.app>
2022-11-25 12:05:12 +0000

    feat: functions for search, query and sorting objects and arrays

--- src/utils.js
+++ src/utils.js
@@ -74,9 +74,9 @@
     }
 
     function parseTextToHtml(text) {
-    let doc = new DOMParser().parseFromString(text, "text/html");
-    if (doc.head.children[0]) return doc.head.children[0];
-    else return doc.body.children[0];
+        let doc = new DOMParser().parseFromString(text, "text/html");
+        if (doc.head.children[0]) return doc.head.children[0];
+        else return doc.body.children[0];
     }
 
     function cssPath(node, container) {
@@ -247,6 +247,228 @@
         }
     }
 
+    function searchData(data, filter) {
+		if (filter && filter.search) {
+			if (filter['search']['type'] == 'and') {
+				data = andSearch(data, filter['search']['value']);
+			} else {
+				data = orSearch(data, filter['search']['value']);
+			}
+			
+			const total = data.length;
+			const startIndex = filter.startIndex;
+			const count = filter.count;
+			let result_data = [];
+			
+			if (startIndex) 
+				data = data.slice(startIndex, total);
+			if (count)
+				data = data.slice(0, count)
+			
+			result_data = data;
+			filter['startIndex'] = startIndex
+			if (count)
+				filter['count'] = count
+			filter['total'] = total
+			return result_data
+		} else {
+			return data
+		}
+	}
+
+	//. or operator
+	function orSearch(results, search) {
+		var tmp
+		if (search && search.length > 0) {
+
+			tmp = results.filter(function(item) {
+				
+				for (var key in item) {
+					var value = item[key];
+					var __status = false;
+					
+					var str_value = value;
+					
+					if (Array.isArray(str_value) || typeof str_value == 'number') {
+						str_value = str_value.toString();
+					}
+					
+					if (typeof str_value == 'string') {
+						str_value = str_value.toUpperCase();
+					}
+
+					for (let i = 0; i < search.length; i++) {
+						if (typeof search[i] == 'string' && typeof str_value == 'string') {
+							if (str_value.indexOf(search[i].toUpperCase()) > -1) {
+								__status = true;
+								break;
+							}
+						} else {
+							if (value == search[i]) {
+								__status = true;
+								break;
+							}
+						}
+					}
+					
+					if (__status) {
+						return true;
+					}
+				}
+				
+				return false;
+			})  
+		} else {
+			tmp = results;
+		}
+		
+		return tmp;
+	}
+
+
+	//. and operator
+	function andSearch(results, search) {
+		var tmp
+		if (search && search.length > 0) {
+					
+			tmp = results.filter(function(item) {
+
+				for (let i = 0; i < search.length; i++) {
+					var __status = false;
+					
+					for (var key in item) {
+						var value = item[key];
+						
+						if (typeof search[i] == 'string') {
+							
+							if (Array.isArray(value) || typeof value == 'number' ) {
+								value = value.toString();
+							} 
+							
+							if (typeof value == 'string') {
+								value = value.toUpperCase();  
+								if (value.indexOf(search[i].toUpperCase()) > -1) {
+									__status = true;
+									break;
+								}
+							}
+							
+						} else {
+							if (value == search[i]) {
+								__status = true;
+								break;
+							}
+						}
+					}
+					
+					if (!__status) {
+						return false;  
+					}
+				}
+				
+				return true;
+			})  
+		} else {
+			tmp = results;
+		}
+		
+		return tmp;
+	}
+		
+	function sortData(data, sorts) {
+		if (!Array.isArray(sorts))
+			sorts = [sorts]
+		for (let sort of sorts) {
+			let name = sort.name
+			if (name) {
+				data.sort((a, b) => {
+					if (!a[name])				
+						a[name] = ''
+					if (!b[name])				
+						b[name] = ''
+					if (sort.type == '-1') {
+						if (sort.valueType == 'number')
+							return b[name] - a[name]
+						else
+							return b[name].localeCompare(a[name])
+					} else {
+						if (sort.valueType == 'number')
+							return a[name] - b[name]
+						else
+							return a[name].localeCompare(b[name])
+					}
+				});
+			}
+		}
+		return data;
+	}	
+
+	function queryData(item, query) {
+		//. $contain, $range, $eq, $ne, $lt, $lte, $gt, $gte, $in, $nin, $geoWithin
+		let flag = true;
+		if (!item || !query) {
+			return false;
+		}
+		if (Array.isArray(item)) return false;
+		for (let i = 0; i < query.length; i++) {
+			let fieldValue = item[query[i].name];
+			if (fieldValue == undefined) 
+				fieldValue = ''
+			let values = query[i].value
+			if (!Array.isArray(values))
+				values = [values] 
+			
+			for (let value of values) {
+				switch (query[i].operator) {
+					case '$contain':
+						if (!fieldValue.includes(value)) 
+							flag = false;
+						break;
+					case '$range':
+						if (value !== null && value !== null) {
+							if (value[0] > fieldValue || value[1] <= fieldValue)
+								flag = false;
+						} else if (item.value[0] == null && value[1] >= fieldValue) {
+							flag = false;
+						} else if (item.value[1] == null && value[0] <= fieldValue) {
+							flag = false;
+						}
+						break;
+					case '$eq':
+						if (fieldValue != value) flag = false; 
+						break;
+					case '$ne':
+						if (fieldValue == value) flag = false;
+						break;
+					case '$lt':
+						if (fieldValue >= value) flag = false;
+						break;
+					case '$lte':
+						if (fieldValue > value) flag = false;
+						break;
+					case '$gt':
+						if (fieldValue <= value) flag = false;
+						break;
+					case '$gte':
+						if (fieldValue < value) flag = false;
+						break;
+					case '$in':
+						if (!Array.isArray(fieldValue) || !fieldValue.some(x => value.includes(x))) flag = false;
+						break;
+					case '$nin':
+						if (Array.isArray(fieldValue) && fieldValue.some(x => value.includes(x))) flag = false;
+						break;
+					default:
+						// if (!Array.isArray(fieldValue) || !fieldValue.some(x => value.includes(x))) flag = false;
+						if (fieldValue && !fieldValue.includes(value)) flag = false; 
+						break;
+				}
+			}
+		}
+		return flag;
+	}
+
+
     // function computeStyles(el, properties) {
     //   let computed = window.getComputedStyle(el);
     //   let result = {};
@@ -277,7 +499,12 @@
         cssPath,
         domParser,
         queryDocumentSelector,
-        queryDocumentSelectorAll
+        queryDocumentSelectorAll,
+        searchData,
+		andSearch,
+		orSearch,
+		sortData,
+		queryData
     }
 
 }));
