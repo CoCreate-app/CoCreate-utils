97fc1fde4c2f4ae018a0c81e38ff4ff8da72c978
frankpagan <frank@cocreate.app>
2023-08-03 17:45:34 -0600

    fix: crud attributes renamed

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "host": "",
     "sources": [
         {
-            "collection": "files",
-            "document": {
+            "array": "files",
+            "object": {
                 "_id": "6143fc94829b690010a5f339",
                 "name": "index.html",
                 "path": "/docs/utils/index.html",
