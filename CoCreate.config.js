d234c1459da3064105ba50cfef9bbdb0fe484cd9
frankpagan <frank@cocreate.app>
2023-11-07 09:47:45 -0600

    fix: update host

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -12,8 +12,7 @@ module.exports = {
                 "pathname": "/docs/utils/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
-                    "*",
-                    "general.cocreate.app"
+                    "*"
                 ],
                 "directory": "utils",
                 "content-type": "{{content-type}}",
